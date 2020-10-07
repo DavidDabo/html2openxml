12e9b54c148937907d731f4579577b579bc97c49
DavidDabo <72400762+DavidDabo@users.noreply.github.com>
2020-10-07 16:39:44 +0200

    Parse html table test

---
+++ test/HtmlToOpenXml.Tests/ParsingTests/HtmlConverter_ParseTable_Tests.cs
@@ -0,0 +1,34 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using HtmlToOpenXml.Tests.Utilities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlToOpenXml.Tests.ParsingTests
+{
+    [TestFixture]
+    public class HtmlConverter_ParseTable_Tests : HtmlConverterTestBase
+    {
+        [TestCase]
+        public void ParseTable_Success()
+        {
+            var html = ResourceHelper.GetFileContentFromResources("Table.html");
+            var dir = @"C:\Users\DavidBolt\source\repos\html2openxml_dave\test\HtmlToOpenXml.Tests\Resources\";
+            var filename = $"Table_{DateTime.Now:yyyyMMddHHmmss}.docx";
+            WordDocumentCreator.Create($"{Path.Combine(dir, filename)}", html);
+        }
+
+        [TestCase]
+        public void ParseTableWithBRs_Success()
+        {
+            var html = ResourceHelper.GetFileContentFromResources("TableWithBRs.html");
+            var dir = @"C:\Users\DavidBolt\source\repos\html2openxml_dave\test\HtmlToOpenXml.Tests\Resources\";
+            var filename = $"Table_{DateTime.Now:yyyyMMddHHmmss}.docx";
+            WordDocumentCreator.Create($"{Path.Combine(dir, filename)}", html);
+        }
+    }
+}
